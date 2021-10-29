31ad3df52b32e9c5a242f3a9d651ea458d8eecde
Srishan Shakya <csunshakya@gmail.com>
2021-10-29 19:57:51 +0545

    update packages and fix lint info/warn messages

--- test/modal_progress_hud_test.dart
+++ test/modal_progress_hud_test.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 void main() {
   group('Modal Progress HUD', () {
-    Widget sut(bool inAsyncCall, Offset? offset) {
+    Widget sut({bool inAsyncCall = false, Offset? offset}) {
       return MaterialApp(
         home: ModalProgressHud(
           inAsyncCall: inAsyncCall,
@@ -15,29 +14,30 @@ void main() {
       );
     }
 
-    testWidgets('should show progress indicator when in async call',
-        (tester) async {
-      const inAsyncCall = true;
-      await tester.pumpWidget(sut(inAsyncCall, null));
+    testWidgets(
+      'should show progress indicator when in async call',
+      (tester) async {
+        await tester.pumpWidget(sut(inAsyncCall: true));
 
-      expect(find.byType(Text), findsOneWidget);
-      expect(find.byType(CircularProgressIndicator), findsOneWidget);
-    });
+        expect(find.byType(Text), findsOneWidget);
+        expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      },
+    );
 
-    testWidgets('should not show progress indicator when not in async call',
-        (tester) async {
-      const inAsyncCall = false;
-      await tester.pumpWidget(sut(inAsyncCall, null));
+    testWidgets(
+      'should not show progress indicator when not in async call',
+      (tester) async {
+        await tester.pumpWidget(sut());
 
-      expect(find.byType(Text), findsOneWidget);
-      expect(find.byType(CircularProgressIndicator), findsNothing);
-    });
+        expect(find.byType(Text), findsOneWidget);
+        expect(find.byType(CircularProgressIndicator), findsNothing);
+      },
+    );
 
     testWidgets('should allow positioning of progress indicator',
         (tester) async {
-      const inAsyncCall = true;
       const offset = Offset(0.1, 0.1);
-      await tester.pumpWidget(sut(inAsyncCall, offset));
+      await tester.pumpWidget(sut(inAsyncCall: true, offset: offset));
 
       expect(find.byType(Positioned), findsOneWidget);
       expect(find.byType(Text), findsOneWidget);
